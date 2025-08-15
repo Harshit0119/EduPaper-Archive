37917ddd00ce0939bb702624dff82e94c2fb71b7
Harshit Chourasia <chouurasiaharshit42@gmail.com>
2025-08-15 13:36:34 +0530

    added notes of M-III

--- study-material.js
+++ study-material.js
@@ -1,16 +1,107 @@
 const studyMaterialLinks = {
   "CSE": {
     "Sem4": {
+      "BT-401": [
+        {
+          type: "PDF",
+          name: "....M-III Unit 1 Notes",
+          url: "materials/CSE/Sem4/M-III/Unit-1 Notes.pdf"
+        },
+        {
+          type: "PDF",
+          name: "....M-III Unit 2 Notes",
+          url: "materials/CSE/Sem4/M-III/Unit-2 Notes.pdf"
+        },
+        {
+          type: "PDF",
+          name: "....M-III Unit 3 Notes",
+          url: "materials/CSE/Sem4/M-III/Unit-3 Notes.pdf"
+        },
+        {
+          type: "PDF",
+          name: "....M-III Unit 5 Notes",
+          url: "materials/CSE/Sem4/M-III/Unit-5 Notes.pdf"
+        },
+        {
+          type: "Mind Map",
+          name: "Unit 1 Revision Sheet",
+          url: "materials/CSE/Sem4/M-III/Unit 1 revision sheet.png"
+        },
+         {
+          type: "Mind Map",
+          name: "Calculus Formula Sheet",
+          url: "materials/CSE/Sem4/M-III/calculus formula sheet.png"
+        }
+      ],
+      "CS-402": [
+        {
+          type: "PDF",
+          name: "ADA Unit 1 & 2 Notes",
+          url: "materials/CSE/Sem4/ADA/UNIT-1 & 2 notes.pdf"
+        },
+        {
+          type: "PDF",
+          name: "ADA Unit 3 & 5 Notes",
+          url: "materials/CSE/Sem4/ADA/Unit 3 & 5 notes.pdf"
+        },
+        {
+          type: "PDF",
+          name: "ADA Unit 4 Notes",
+          url: "materials/CSE/Sem4/ADA/unit 4 notes.pdf"
+        },
+        {
+          type: "Mind Map",
+          name: "Unit 1 Short Notes",
+          url: "materials/CSE/Sem4/ADA/unit 1 short note.png"
+        },
+        {
+          type: "Mind Map",
+          name: "Unit 2 Short Note",
+          url: "materials/CSE/Sem4/ADA/unit 2 short note.png"
+        }
+      ],
+      "CS-403": [
+        {
+          type: "PDF",
+          name: "SE Unit 2 Notes",
+          url: "materials/CSE/Sem4/SE/Unit 2 Note.pdf"
+        },
+        {
+          type: "PDF",
+          name: "SE Unit 3 Notes",
+          url: "materials/CSE/Sem4/SE/Unit 3 Note.pdf"
+        },
+        {
+          type: "PDF",
+          name: "SE Unit 4 Notes",
+          url: "materials/CSE/Sem4/SE/unit 4 Note.pdf"
+        },
+         {
+          type: "PDF",
+          name: "SE Unit 5 Notes",
+          url: "materials/CSE/Sem4/SE/unit 5 Note.pdf"
+        },
+        {
+          type: "Mind Map",
+          name: "Unit 1 Mind Map",
+          url: "materials/CSE/Sem4/SE/unit 1_Mind map.png"
+        },
+        {
+          type: "Mind Map",
+          name: "Unit 2 Mind Map",
+          url: "materials/CSE/Sem4/SE/unit 2_Mind map.png"
+        },
+        {
+          type: "Mind Map",
+          name: "Unit 3 Mind Map",
+          url: "materials/CSE/Sem4/SE/unit 3_Mind map.png"
+        }
+      ],
       "CS-404": [
         {
           type: "PDF",
           name: "COA Unit 1",
           url: "materials/CSE/Sem4/COA/Unit_1.pdf"
-        },
-        {
-          type: "Mind Map",
-          name: "COA Mind Map",
-          url: "materials/CSE/Sem4/COA/COA_MindMap.png"
         },
         {
           type: "PDF",
@@ -27,20 +118,192 @@ const studyMaterialLinks = {
           name: "COA Unit 5",
           url: "materials/CSE/Sem4/COA/Unit_5.pdf"
         }
-       
       ],
-      // "ADA": [
-      //   {
-      //     type: "PDF",
-      //     name: "ADA Unit 1",
-      //     url: "materials/CSE/Sem4/ADA/ADA_UNIT_1.pdf"
-      //   },
-      //   {
-      //     type: "Mind Map",
-      //     name: "ADA Mind Map",
-      //     url: "materials/CSE/Sem4/ADA/ADA_MindMap.png"
-      //   }
-      // ]
     }
-  }
-};
+  },
+
+  "ECE": {
+    "Sem3": {
+      "BT-301": [
+        {
+          type: "PDF",
+          name: "....M-III Unit 1 Notes",
+          url: "materials/CSE/Sem4/M-III/Unit-1 Notes.pdf"
+        },
+        {
+          type: "PDF",
+          name: "....M-III Unit 2 Notes",
+          url: "materials/CSE/Sem4/M-III/Unit-2 Notes.pdf"
+        },
+        {
+          type: "PDF",
+          name: "....M-III Unit 3 Notes",
+          url: "materials/CSE/Sem4/M-III/Unit-3 Notes.pdf"
+        },
+        {
+          type: "PDF",
+          name: "....M-III Unit 5 Notes",
+          url: "materials/CSE/Sem4/M-III/Unit-5 Notes.pdf"
+        },
+        {
+          type: "Mind Map",
+          name: "Unit 1 Revision Sheet",
+          url: "materials/CSE/Sem4/M-III/Unit 1 revision sheet.png"
+        },
+         {
+          type: "Mind Map",
+          name: "Calculus Formula Sheet",
+          url: "materials/CSE/Sem4/M-III/calculus formula sheet.png"
+        }
+      ],
+    }
+  },
+
+  "ME": {
+    "Sem3": {
+      "BT-301": [
+        {
+          type: "PDF",
+          name: "....M-III Unit 1 Notes",
+          url: "materials/CSE/Sem4/M-III/Unit-1 Notes.pdf"
+        },
+        {
+          type: "PDF",
+          name: "....M-III Unit 2 Notes",
+          url: "materials/CSE/Sem4/M-III/Unit-2 Notes.pdf"
+        },
+        {
+          type: "PDF",
+          name: "....M-III Unit 3 Notes",
+          url: "materials/CSE/Sem4/M-III/Unit-3 Notes.pdf"
+        },
+        {
+          type: "PDF",
+          name: "....M-III Unit 5 Notes",
+          url: "materials/CSE/Sem4/M-III/Unit-5 Notes.pdf"
+        },
+        {
+          type: "Mind Map",
+          name: "Unit 1 Revision Sheet",
+          url: "materials/CSE/Sem4/M-III/Unit 1 revision sheet.png"
+        },
+         {
+          type: "Mind Map",
+          name: "Calculus Formula Sheet",
+          url: "materials/CSE/Sem4/M-III/calculus formula sheet.png"
+        }
+      ],
+    }
+  },
+
+  "CE": {
+    "Sem3": {
+      "BT-301": [
+        {
+          type: "PDF",
+          name: "....M-III Unit 1 Notes",
+          url: "materials/CSE/Sem4/M-III/Unit-1 Notes.pdf"
+        },
+        {
+          type: "PDF",
+          name: "....M-III Unit 2 Notes",
+          url: "materials/CSE/Sem4/M-III/Unit-2 Notes.pdf"
+        },
+        {
+          type: "PDF",
+          name: "....M-III Unit 3 Notes",
+          url: "materials/CSE/Sem4/M-III/Unit-3 Notes.pdf"
+        },
+        {
+          type: "PDF",
+          name: "....M-III Unit 5 Notes",
+          url: "materials/CSE/Sem4/M-III/Unit-5 Notes.pdf"
+        },
+        {
+          type: "Mind Map",
+          name: "Unit 1 Revision Sheet",
+          url: "materials/CSE/Sem4/M-III/Unit 1 revision sheet.png"
+        },
+         {
+          type: "Mind Map",
+          name: "Calculus Formula Sheet",
+          url: "materials/CSE/Sem4/M-III/calculus formula sheet.png"
+        }
+      ],
+    }
+  },
+
+  "EX": {
+    "Sem4": {
+      "BT-401": [
+        {
+          type: "PDF",
+          name: "....M-III Unit 1 Notes",
+          url: "materials/CSE/Sem4/M-III/Unit-1 Notes.pdf"
+        },
+        {
+          type: "PDF",
+          name: "....M-III Unit 2 Notes",
+          url: "materials/CSE/Sem4/M-III/Unit-2 Notes.pdf"
+        },
+        {
+          type: "PDF",
+          name: "....M-III Unit 3 Notes",
+          url: "materials/CSE/Sem4/M-III/Unit-3 Notes.pdf"
+        },
+        {
+          type: "PDF",
+          name: "....M-III Unit 5 Notes",
+          url: "materials/CSE/Sem4/M-III/Unit-5 Notes.pdf"
+        },
+        {
+          type: "Mind Map",
+          name: "Unit 1 Revision Sheet",
+          url: "materials/CSE/Sem4/M-III/Unit 1 revision sheet.png"
+        },
+         {
+          type: "Mind Map",
+          name: "Calculus Formula Sheet",
+          url: "materials/CSE/Sem4/M-III/calculus formula sheet.png"
+        }
+      ],
+    }
+  },
+
+  "CSIT": {
+    "Sem4": {
+      "BT-401": [
+        {
+          type: "PDF",
+          name: "....M-III Unit 1 Notes",
+          url: "materials/CSE/Sem4/M-III/Unit-1 Notes.pdf"
+        },
+        {
+          type: "PDF",
+          name: "....M-III Unit 2 Notes",
+          url: "materials/CSE/Sem4/M-III/Unit-2 Notes.pdf"
+        },
+        {
+          type: "PDF",
+          name: "....M-III Unit 3 Notes",
+          url: "materials/CSE/Sem4/M-III/Unit-3 Notes.pdf"
+        },
+        {
+          type: "PDF",
+          name: "....M-III Unit 5 Notes",
+          url: "materials/CSE/Sem4/M-III/Unit-5 Notes.pdf"
+        },
+        {
+          type: "Mind Map",
+          name: "Unit 1 Revision Sheet",
+          url: "materials/CSE/Sem4/M-III/Unit 1 revision sheet.png"
+        },
+         {
+          type: "Mind Map",
+          name: "Calculus Formula Sheet",
+          url: "materials/CSE/Sem4/M-III/calculus formula sheet.png"
+        }
+      ],
+    }
+  },
+}
